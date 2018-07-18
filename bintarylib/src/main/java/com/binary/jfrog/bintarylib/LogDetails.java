0f23d1a1de7ce18a116717570bc2dc0801bd4331
Rajesh <rajesh@speedkyat.com>
2018-07-18 17:41:16 +0630

    First committ

---
+++ bintarylib/src/main/java/com/binary/jfrog/bintarylib/LogDetails.java
@@ -0,0 +1,17 @@
+package com.binary.jfrog.bintarylib;
+
+import android.util.Log;
+
+/**
+ * Created by super on 7/18/18.
+ */
+
+public class LogDetails {
+
+
+    public void data(String msg)
+    {
+        Log.d("Data_value--",msg);
+        System.out.println("Data_value--"+msg);
+    }
+}
